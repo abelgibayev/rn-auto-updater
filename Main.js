6b7353dcf058fdd2c9b48cf0adc83588fe32b74c
Azat <belg.azat@gmail.com>
2019-10-28 16:38:09 +0300

    finished auto-update featuer

--- Main.js
+++ Main.js
@@ -1,24 +1,54 @@
 import React from 'react';
 const RNFS = require('react-native-fs');
 
-import {View, Text} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
 export default class Main extends React.Component {
-  componentDidMount(): void {
+  state = {
+    curActiveBundle: '',
+  };
+
+  _onBlueButtonPress = async () => {
     RNFS.downloadFile({
-      fromUrl: 'http://10.10.15.245:8900/applicationBundle.js',
-      toFile: RNFS.DocumentDirectoryPath + '/applicationBundle.js',
+      fromUrl: 'http://10.10.15.245:8900/index.android.blue.bundle',
+      toFile: RNFS.DocumentDirectoryPath + '/ReactNativeDevBundle.js',
       background: false,
     })
       .promise.then(res => console.log(res))
       .catch(err => console.log(err));
-  }
+    console.log('loaded red file');
+  };
+
+  _onRedButtonPress = async () => {
+    RNFS.downloadFile({
+      fromUrl: 'http://10.10.15.245:8900/index.android.red.bundle',
+      toFile: RNFS.DocumentDirectoryPath + '/ReactNativeDevBundle.js',
+      background: false,
+    })
+      .promise.then(res => console.log(res))
+      .catch(err => console.log(err));
+    console.log('loaded blue file');
+  };
 
   render() {
     return (
       <View>
-        <Text>Hello world</Text>
+        <Text style={styles.red}>Hello world</Text>
+        <Text style={styles.red}>Hello world</Text>
+        <Text style={styles.red}>{this.state.curActiveBundle}</Text>
+        <Button onPress={this._onReloadButtonPress} title="LOAD" />
+        <Button onPress={this._onRedButtonPress} title="red" />
+        <Button onPress={this._onBlueButtonPress} title="blue" />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  red: {
+    color: 'red',
+  },
+  blue: {
+    color: 'blue',
+  },
+});
